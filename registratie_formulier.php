84b95bbbbbedd9511d9afd78348c8252b575fdc4
DaOlcia <aleksandra.lepczynska@o2.pl>
2023-03-10 13:34:35 +0100

    fgh

---
+++ registratie_formulier.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Registratieformulier</h1>
+    <form action="">
+        <label for="voornaam">Voornaam</label>
+        <input type="text" id="voornaam" name="voornaam">
+        <label for="achternaam">Achternaam</label>
+        <input type="text" id="achternaam" name="achternaam">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email">
+        <label for="gebruikersnaam">Username</label>
+        <input type="text" id="gebruikersnaam" name="gebruikersnaam">
+        <label for="wachtwoord">Wachtwoord</label>
+        <input type="password" id="wachtwoord" name="wachtwoord">
+        <label for="wachtwoord">Wachtwoord herhalen</label>
+        <input type="password" id="wachtwoord" name="wachtwoord">
+        <input type="submit" value="Registreren">
+    </form>
+</body>
+</html>
